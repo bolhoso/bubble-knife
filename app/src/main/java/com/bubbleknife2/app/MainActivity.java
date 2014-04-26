b35b7686dbb9d12de1b3bc5ec9d863d74b481e32
lucas <lucasrosada@gmail.com>
2014-04-26 03:33:28 -0300

    Adding the actual injection!

--- app/src/main/java/com/bubbleknife2/app/MainActivity.java
+++ app/src/main/java/com/bubbleknife2/app/MainActivity.java
@@ -2,8 +2,10 @@ package com.bubbleknife2.app;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bubbleknife.InjectThis;
 import com.bubbleknife.Injector;
@@ -25,7 +27,15 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Injector.inject(MainActivity.class);
+        Injector.inject(this);
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Hooray! I've been injected!", Toast.LENGTH_LONG).show();
+            }
+        });
+
+
     }
 
 }
--- bubble-knife/src/main/java/com/bubbleknife/Injector.java
+++ bubble-knife/src/main/java/com/bubbleknife/Injector.java
@@ -1,13 +1,26 @@
 package com.bubbleknife;
 
 
+import android.app.Activity;
+
+import java.lang.reflect.Method;
+
 public class Injector {
     private Injector() {
 
     }
 
-    public static void inject(Class<?> activity) {
-
+    public static void inject(Activity activity) {
+        try {
+            Class<?> injector = Class.forName(activity.getClass().getName() + InjectorProcessor.SUFFIX);
+            Method inject = injector.getMethod("inject", activity.getClass());
+            inject.invoke(null, activity);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("hmm something wrong! " + e);
+        }
     }
+
+
 
 }
